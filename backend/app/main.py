ce89ff24f47754b14b89af576ba273aa5a4005bb
MparthXD <pmodi8695@gmail.com>
2026-03-02 14:28:24 +0530

    url

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "ripple-media-app.vercel.app",  # 👈 add your Vercel URL here after deploying frontend
+        "https://ripple-media-app.vercel.app",  # 👈 add your Vercel URL here after deploying frontend
     ],
     allow_credentials=True,
     allow_methods=["*"],
